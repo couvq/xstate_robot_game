a36ada5ae3588adc5e1ecf26b66527e4468edea2
couvq <quentincouvelaire@gmail.com>
2026-07-12 18:31:58 -0400

    non-valid move test

--- src/machines/gameMachine.test.ts
+++ src/machines/gameMachine.test.ts
@@ -3,19 +3,22 @@ import { createActor, type ActorRefFrom } from "xstate";
 import { gameMachine, type Position } from "./gameMachine";
 import { GAME_TIME_SECS, INITIAL_SCORE } from "../constants";
 
+const initialRobotPos: Position = [0, 0];
+
 describe("game machine actor", () => {
   let gameActor: ActorRefFrom<typeof gameMachine>;
 
   beforeEach(() => {
     vi.stubGlobal("window", { addEventListener: () => {} });
-    vi.useFakeTimers()
+    vi.useFakeTimers();
     gameActor = createActor(gameMachine);
     gameActor.start();
-  })
-  
+  });
+
   afterEach(() => {
     vi.clearAllMocks();
-  })
+    gameActor.stop();
+  });
 
   test("should initially be in playing state with proper initial score and countdown time when game starts", () => {
     const initialSnapshot = gameActor.getSnapshot();
@@ -26,23 +29,41 @@ describe("game machine actor", () => {
 
   test("should update score when collision is detected", () => {
     // force robot and candy to be next to each other on the board, initial position is determined at random so need to make this deterministic
-    const initialRobotPos: Position = [0, 0]
-    const initialCandyPos: Position = [0, 1]
+    const initialCandyPos: Position = [0, 1];
     gameActor.getSnapshot().context.robotPosition = initialRobotPos;
     gameActor.getSnapshot().context.candyPosition = initialCandyPos;
-    expect(gameActor.getSnapshot().context.score).toBe(0)
-    gameActor.send({ type: "move", direction: "right" }); 
-    expect(gameActor.getSnapshot().context.robotPosition).toEqual(initialCandyPos) // collision
-    // candy position is reset after collision, guaranteed not to be in the same position as the robot 
-    expect(gameActor.getSnapshot().context.robotPosition).not.toEqual(gameActor.getSnapshot().context.candyPosition)
-    expect(gameActor.getSnapshot().context.score).toBe(1)
+    expect(gameActor.getSnapshot().context.score).toBe(INITIAL_SCORE);
+    gameActor.send({ type: "move", direction: "right" });
+    expect(gameActor.getSnapshot().context.robotPosition).toEqual(
+      initialCandyPos
+    ); // collision
+    // candy position is reset after collision, guaranteed not to be in the same position as the robot
+    expect(gameActor.getSnapshot().context.robotPosition).not.toEqual(
+      gameActor.getSnapshot().context.candyPosition
+    );
+    expect(gameActor.getSnapshot().context.score).toBe(INITIAL_SCORE + 1);
   });
 
-  test('game should finish once time runs out', () => {
-    expect(gameActor.getSnapshot().context.timeRemainingSecs).toBeGreaterThan(0)
-    expect(gameActor.getSnapshot().matches('playing')).toBe(true)
-    vi.advanceTimersByTime(GAME_TIME_SECS * 1000)
-    expect(gameActor.getSnapshot().matches('finished')).toBe(true)
-    expect(gameActor.getSnapshot().context.timeRemainingSecs).toBe(0)
-  })
+  test("should not allow moving off the board", () => {
+    gameActor.getSnapshot().context.robotPosition = initialRobotPos; // upper left corner
+    gameActor.send({ type: "move", direction: "left" }); // non-valid move
+    gameActor.send({ type: "move", direction: "up" }); // non-valid move
+    expect(gameActor.getSnapshot().context.robotPosition).toEqual(
+      initialRobotPos
+    );
+  });
+
+  test("game should finish once time runs out", () => {
+    expect(gameActor.getSnapshot().context.timeRemainingSecs).toBeGreaterThan(
+      0
+    );
+    expect(gameActor.getSnapshot().matches("playing")).toBe(true);
+    vi.advanceTimersByTime(GAME_TIME_SECS * 1000);
+    expect(gameActor.getSnapshot().matches("finished")).toBe(true);
+    expect(gameActor.getSnapshot().context.timeRemainingSecs).toBe(0);
+  });
+
+  test("should be able to restart game from finished", () => {
+      // TODO
+  });
 });
