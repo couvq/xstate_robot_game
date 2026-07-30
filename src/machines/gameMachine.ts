baafff44793080302644fed408556ee8add5a148
couvq <quentincouvelaire@gmail.com>
2026-07-30 13:50:38 -0400

    fixed bug where candies did not load on each level after the first one

--- src/machines/gameMachine.ts
+++ src/machines/gameMachine.ts
@@ -1,4 +1,4 @@
-import { assign, createActor, setup, stopChild } from "xstate";
+import { assign, createActor, setup, stopChild, type Spawner } from "xstate";
 import { candyActor, gameTimeActor, robotActor } from "../actors";
 import { BOARD_SIZE, GAME_TIME_SECS, INITIAL_SCORE } from "../constants";
 import type { GameContext, GameEvent, MoveRobotEvent } from "../types";
@@ -11,16 +11,22 @@ import {
   hasCollision,
 } from "../utils/positionUtils";
 
-const createInitialContext = (currentLevel?: number): GameContext => {
+const createInitialContext = (
+  spawn: Spawner<any>,
+  currentLevel?: number
+): GameContext => {
   const wallPositions =
     currentLevel !== undefined
       ? LEVEL_CONFIGS[currentLevel].wallPositions
       : LEVEL_CONFIGS[0].wallPositions;
   const robotPos = getInitialRobotPosition(wallPositions);
+  const candies = new Map([
+    [spawn("candyActor", {}), getNewCandyPosition(robotPos, wallPositions)],
+  ]);
 
   return {
     robotPosition: robotPos,
-    candies: new Map(),
+    candies,
     wallPositions,
     level: currentLevel ?? 0,
     score: INITIAL_SCORE,
@@ -34,7 +40,7 @@ export const gameMachine = setup({
     events: {} as GameEvent,
   },
   guards: {
-    isValidMove: ({ context, event }) => {
+    isValidRobotMove: ({ context, event }) => {
       const potentialNextPosition = getNextPosition(
         context,
         event as MoveRobotEvent
@@ -61,7 +67,7 @@ export const gameMachine = setup({
       robotPosition: ({ context, event }) =>
         getNextPosition(context, event as MoveRobotEvent),
     }),
-    checkCollisions: assign(({ context, spawn }) => {
+    checkRobotCollisions: assign(({ context, spawn }) => {
       let score = context.score;
       const nextCandies = new Map(context.candies);
       for (const [candyRef, candyPosition] of context.candies.entries()) {
@@ -70,7 +76,7 @@ export const gameMachine = setup({
           nextCandies.delete(candyRef);
           stopChild(candyRef);
           nextCandies.set(
-            spawn(candyActor, {}),
+            spawn("candyActor", {}),
             getNewCandyPosition(context.robotPosition, context.wallPositions)
           );
         }
@@ -81,37 +87,38 @@ export const gameMachine = setup({
     decrementGameTime: assign({
       timeRemainingSecs: ({ context }) => context.timeRemainingSecs - 1,
     }),
-    resetGame: assign(() => createInitialContext()),
-    incrementLevel: assign(({ context }) =>
-      createInitialContext(context.level + 1)
-    ),
-    spawnCandy: assign({
-      candies: ({ context, spawn }) =>
-        context.candies.set(
-          spawn(candyActor, {}),
-          getNewCandyPosition(context.robotPosition, context.wallPositions)
-        ),
+    resetGame: assign(({ context, spawn }) => {
+      [...context.candies.keys()].forEach((candyActorRef) =>
+        stopChild(candyActorRef)
+      );
+      return createInitialContext(spawn);
+    }),
+    incrementLevel: assign(({ context, spawn }) => {
+      [...context.candies.keys()].forEach((candyActorRef) =>
+        stopChild(candyActorRef)
+      );
+      return createInitialContext(spawn, context.level + 1);
     }),
   },
   actors: {
-    robotActor,
     gameTimeActor,
+    robotActor,
+    candyActor,
   },
 }).createMachine({
   /** @xstate-layout N4IgpgJg5mDOIC5RQIYFswDoAOAbFAngJYB2UAxGgPYBuYA2gAwC6io2VsRALkVSWxAAPRADZGmAKyiATABYAnKMmSAjAGZGShQBoQBRKoDsRzAsnrVqgByM76mbfUBfZ3tQYc+YmXIBjKgBXEm4IKgB3EiZWJBAOLl5+QREEUVVMUVE5RlEHI1VJIwUZVT0DBGt0xQUa0SNrbLlJeVd3dCw8QlIKaMF4nj4BWJTHCSNGSwLRYoUjRVEyxHUjSUxVLWX1c0ZrNVFWkA8sADNSIlgAC0hyACc4bhQb7l7Y-sSh0BSHdUx1SQV1HVrJVrOI-osEA45BkNtZltlRNYSgcjphTiRzlcIOQSGAhNwADJgOi4F7sTgDJLDRByTJrcyaOQyGSFdRyBoQmRGH6qYr5SRyJnLIyuNwgEhUCBwQRHPoU97JRAAWlK+mVokwdi1qiZcPUfzkIrFqM6PigcoSg0VCENEM0Gr+ANsjBk2v2xvaaLOl0gFspH2ENL+UkY9RdmgUgoRdv19K5BUFMmFoucQA */
   id: "game",
   initial: "playing",
-  context: createInitialContext(),
-  entry: [{ type: "spawnCandy" }],
+  context: ({ spawn }) => createInitialContext(spawn),
   states: {
     playing: {
       invoke: [{ src: "robotActor" }, { src: "gameTimeActor" }],
       on: {
         moveRobot: {
-          guard: "isValidMove",
+          guard: "isValidRobotMove",
           actions: [
             { type: "updateRobotPosition" },
-            { type: "checkCollisions" },
+            { type: "checkRobotCollisions" },
           ],
         },
         countdown: {
